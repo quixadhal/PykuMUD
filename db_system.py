b37943449ea1f0945fcb38f87cefb169c287be92
Dread Quixadhal <quixadhal@gmail.com>
2015-03-01 03:43:39 -0500

    Added trivial support for using SQLite as a data store.

---
+++ db_system.py
@@ -0,0 +1,29 @@
+__author__ = 'quixadhal'
+import os
+import sys
+import log_system
+from peewee import *
+
+
+logger = log_system.init_logging()
+sys.path.append(os.getcwd())
+master_database = SqliteDatabase('pyku.db')
+
+
+class DataBase(Model):
+    class Meta:
+        database = master_database
+
+
+class Option(DataBase):
+    name = CharField(unique=True)
+    val = CharField()
+
+
+def init_db():
+    master_database.connect()
+    if not Option.table_exists():
+        master_database.create_tables([Option], True)
+        port = Option.create(name='port', val='4400')
+        port.save()
+        logger.boot('Database initialized.')
--- pykumud.py
+++ pykumud.py
@@ -2,19 +2,25 @@ __author__ = 'quixadhal'
 import os
 import sys
 import time
-import log_system
-
-
-sys.path.append(os.getcwd())
-logger = log_system.init_logging()
-
 import sysutils
+import log_system
+import db_system
+from db_system import Option
+
+
+logger = log_system.init_logging()
+sys.path.append(os.getcwd())
 
 
 if __name__ == '__main__':
     logger.boot('System booting.')
     snapshot = sysutils.ResourceSnapshot()
     logger.info(snapshot.log_data())
+    db_system.init_db()
+    snapshot = sysutils.ResourceSnapshot()
+    logger.info(snapshot.log_data())
+    port = int(Option.get(Option.name == 'port').val)
+    logger.boot('Port number is %d', port)
     time.sleep(1)
     snapshot = sysutils.ResourceSnapshot()
     logger.info(snapshot.log_data())
