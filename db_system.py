0a4defe12dbeed4850171aab8efd69ca9707adc1
Dread Quixadhal <quixadhal@gmail.com>
2015-03-06 06:12:01 -0500

    Created a braindead, but serviceable upgrade system.  The code and database now have version numbers.  If the db version is < the code version, a function is called to attempt an auto-upgrade.

--- db_system.py
+++ db_system.py
@@ -27,13 +27,26 @@ def init_db():
     master_database.connect()
     if not Option.table_exists():
         master_database.create_tables([Option], True)
+        db_version = Option.create(name='db_version', val=serialization.pack(1))
         port = Option.create(name='port', val=serialization.pack(4400))
         o2 = Option.create(name='o2', val=serialization.pack('stuff'))
         o3 = Option.create(name='o3', val=serialization.pack(False))
         example = serialization.ExampleThing()
         thing = Option.create(name='thing', val=serialization.pack(example))
+        db_version.save()
         port.save()
         o2.save()
         o3.save()
         thing.save()
         logger.boot('Database initialized.')
+
+
+def upgrade_db(from_version: int, to_version: int):
+    if from_version == 1:
+        shiny = Option.create(name='shiny', val=serialization.pack('all your base are belong to us'))
+        shiny.save()
+        db_version_obj = Option.get(Option.name == 'db_version')
+        db_version_obj.val = serialization.pack(to_version)
+        db_version_obj.save()
+        return True
+    return False
--- pykumud.py
+++ pykumud.py
@@ -13,6 +13,7 @@ from db_system import Option
 
 logger = log_system.init_logging()
 sys.path.append(os.getcwd())
+code_version = 2
 
 if __name__ == '__main__':
     logger.boot('System booting.')
@@ -22,14 +23,25 @@ if __name__ == '__main__':
     snapshot = sysutils.ResourceSnapshot()
     logger.info(snapshot.log_data())
 
+    db_version = serialization.unpack(Option.get(Option.name == 'db_version').val)
+    logger.boot('Version number is %d', db_version)
+
+    if db_version < code_version:
+        logger.boot('Old database version, attempting to upgrade.')
+        if db_system.upgrade_db(db_version, code_version):
+            db_version = serialization.unpack(Option.get(Option.name == 'db_version').val)
+            logger.boot('Success!  New version is %d', db_version)
+
     port = serialization.unpack(Option.get(Option.name == 'port').val)
     o2 = serialization.unpack(Option.get(Option.name == 'o2').val)
     o3 = serialization.unpack(Option.get(Option.name == 'o3').val)
     example = serialization.unpack(Option.get(Option.name == 'thing').val)
+    shiny = serialization.unpack(Option.get(Option.name == 'shiny').val)
     logger.boot('Port number is %d', port)
     logger.boot('o2 string is %s', o2)
     logger.boot('o3 boolean is %s', o3)
     logger.boot('example.foo is %s', example.foo)
+    logger.boot('shiny is %s', shiny)
     if o3:
         logger.boot('o3 is true')
 
